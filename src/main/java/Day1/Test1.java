0421599f25a1d11a14586bd3eb04374abd22d3fd
hailin chen <hchen256@my.bcit.ca>
2022-05-31 23:29:03 -0700

    Javadoc

--- src/main/java/Day1/Test1.java
+++ src/main/java/Day1/Test1.java
@@ -1,8 +1,33 @@
 package Day1;
 
+/**
+ * 1. One public class in each .java file
+ * 2. The class name should be the same as the file name.
+ */
 public class Test1 {
+    /**
+     * main function: entrance -- __name__ == '__main__'
+     * Only one entrance allowed
+     */
     public static void main(String[] args) {
-//        main function: entrance -- __name__ == '__main__'
+
+
+    }
+
+    /**
+     * Javadoc test
+     *  javadoc -d (directory name) -author -version
+     *  save javadoc to directory
+     * shortcut - shift+Enter
+     */
+    static class A {
+        int sample = 'A';
+    }
+
+    /**
+     * Select Tools - Generate javadoc to define output dir and scope
+     */
+    public void start(){
 
     }
 }
