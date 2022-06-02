3773ee525078cd9ab56011111cd15d6ac93c5653
hailin chen <hchen256@my.bcit.ca>
2022-06-01 21:44:36 -0700

    Switch selection

---
+++ src/main/java/Day2/Day2.java
@@ -0,0 +1,57 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Day2 {
+  public static void main (String[] args) {
+    /*
+    >> is arithmetic shift right, >>> is logical shift right.
+    In an arithmetic shift, the sign bit is extended to preserve the signedness of the number.
+     */
+//    int x = -2; // 1111 1110
+//    int y = x >> 1; // 1111 1111
+//    int z = x >>> 1; // 0111 1111
+//    System.out.println(y);
+//    System.out.println(z);
+    
+    /*
+    Scanner
+     */
+//    java.util.Scanner sc = new java.util.Scanner(System.in);
+//    int userInput = sc.nextInt();
+//    System.out.println(userInput);
+    
+    /*
+    selection: using if and switch
+     */
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+//    int age = sc.nextInt();
+//    if (age < 0 || age > 150) {
+//      System.out.println("Age not eligible");
+//    } else {
+//      if (age <= 5) {
+//        System.out.println("Infant");
+//      } else if (age <= 15) {
+//        System.out.println("Teenager");
+//      } else if (age <= 60) {
+//        System.out.println("Adult");
+//      } else {
+//        System.out.println("TOO old!");
+//      }
+//    }
+    // switch value only support int type (short char) and string
+//    double score  = sc.nextDouble();
+//    int grade = (int) score / 10;
+//    String str = "Not Pass";
+//    switch (grade) {
+//      case 10, 9 -> str = "Excellent";
+//      case 8 -> str = "Good";
+//      case 7 -> str = "Mid";
+//      case 6 -> str = "BAD";
+//    }
+//    System.out.println(str);
+  
+  
+  
+  }
+}
