731b6e79caeb13321a40e6e7ec7fdee3f08d9e8b
hailin chen <hchen256@my.bcit.ca>
2022-09-19 14:58:03 -0700

    Updated java basics

---
+++ src/main/java/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class test {
+  
+  public static void main (String[] args) {
+    BigDecimal num1 = BigDecimal.valueOf(10);
+    BigInteger num2 = BigInteger.valueOf(3);
+//    BigInteger num3 = num1.multiply(num2);
+  
+  }
+  
+}
